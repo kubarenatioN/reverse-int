e36d84aae6127b1a1236f3a439d6e31383e6ca2d
Nick <65170141+kubarenatioN@users.noreply.github.com>
2021-01-22 12:00:21 +0300

    feat: implement reverse int algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = reverse
+
+function reverse(n) {
+  if(n < 0) n *= -1
+  let strFromInt = n.toString()
+  let reversedStr = strFromInt.split('').reverse()
+  while(reversedStr[0] == '0'){
+    reversedStr.shift()
+  }
+  reversedStr = reversedStr.join('')
+  return +reversedStr
 }
